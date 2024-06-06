b184641bfea2a1991187d99ef5e363385bd5dd5b
Shreekanth99 <82930841+Shreekanth99@users.noreply.github.com>
2024-06-06 18:52:43 +0530

    Update the caluclations

--- app/controllers/tax_controller.rb
+++ app/controllers/tax_controller.rb
@@ -10,29 +10,27 @@ class TaxController < ApplicationController
 
     def calculate_tax(employee)
         start_date = Date.new(Date.today.year - 1, 4, 1)
-        end_date = Date.new(Date.today.year, 4, 1)
-        start_date = employee.doj if employee.doj > start_date
-        months_worked = ((end_date.year * 12 + end_date.month) - (start_date.year * 12 + start_date.month)).to_f
-        if employee.doj > start_date
-            months_worked -= 1
-        end
-        total_salary = employee.salary * months_worked
-        tax_amount = case total_salary
+        end_date = Date.new(Date.today.year, 3, 31)
+        anual_salary = employee.salary * 12
+        per_day = (employee.salary * 12)/365
+        worked_on_days = (end_date - employee.doj).to_i
+        actual_salary = per_day * worked_on_days
+        tax_amount = case actual_salary
                      when 0..250000
                         0
                      when 250001..500000
-                        (total_salary - 250000) * 0.05
+                        actual_salary * 0.05
                      when 500001..1000000
-                        12500 + (total_salary - 500000) * 0.1
+                        actual_salary * 0.1
                      else
-                        62500 + (total_salary - 1000000) * 0.2
+                        actual_salary * 0.2
                      end
-        cess_amount = (total_salary > 2500000) ? (total_salary - 2500000) * 0.02 : 0
+        cess_amount = (actual_salary > 2500000) ? (actual_salary - 2500000) * 0.02 : 0
         {
             employee_code: employee.employee_code,
             first_name: employee.first_name,
             last_name: employee.last_name,
-            yearly_salary: total_salary,
+            yearly_salary: anual_salary,
             tax_amount: tax_amount,
             cess_amount: cess_amount
         }
